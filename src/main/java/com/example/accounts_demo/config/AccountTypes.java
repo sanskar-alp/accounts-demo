794ad0ca5af79d2225a20bbd26121024cc136186
sanskargoel <sanskar.goel@alptech.io>
2026-01-16 14:09:57 +0530

    Test commit

--- src/main/java/com/example/accounts_demo/config/AccountTypes.java
+++ src/main/java/com/example/accounts_demo/config/AccountTypes.java
@@ -3,7 +3,6 @@ package com.example.accounts_demo.config;
 
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.CollectionUtils;
 import java.util.List;
